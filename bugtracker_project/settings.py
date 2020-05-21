ee233bed7d476a263c33a2f17007263fdae32dd5
Tim Rohweder <trohweder85@gmail.com>
2020-05-20 22:41:34 -0700

    add environment variables

--- bugtracker_project/settings.py
+++ bugtracker_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h-+j0_%p^n*w^ag3lje2(fk6!fzac&e_3#g20do^y-t@j++b#t'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
