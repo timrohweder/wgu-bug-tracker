57f78195840dd48cb09feef41af02c3c959f1151
Tim Rohweder <trohweder85@gmail.com>
2020-05-27 19:43:09 -0700

    fix allowed hosts typo

--- bugtracker_project/settings.py
+++ bugtracker_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['https://wgu-bugtracker-project.herokuapp.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['wgu-bugtracker-project.herokuapp.com/', 'localhost', '127.0.0.1']
 
 
 # Application definition
